0db888399b6d28362db66d89a7e1db582d63276e
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-21 15:46:30 +0900

    birth, number 삭제

--- models/userdb.js
+++ models/userdb.js
@@ -3,9 +3,7 @@ userdb = [{
     userPw: 'admin',
     userName: '관리자',
     nickname: '관리관리',
-    birth: '19000101',
     gender: '여자',
-    number: '0211112222',
     phoneNumber: '01011112222',
     level: 1,
     active: 1
