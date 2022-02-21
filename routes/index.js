35ae6f680f9f683e5b0165009366ea299da11dbf
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-21 15:10:08 +0900

    routes>index에 user라우터 생성

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
+const { application } = require('express')
 const express = require('express')
 const router = express.Router()
 const user = require('../models/userdb')
 const boardRouter = require('./board/index')
+const userRouter = require('./user/index')
 
 
 // const Access = (req,res,next)=>{
@@ -22,5 +24,10 @@ router.get('/',(req,res)=>{
 
 //board 라우터 모음
 router.use('/board',boardRouter)
+
+
+//user
+router.use('/user',userRouter)
+
 
 module.exports = router
