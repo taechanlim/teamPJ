b3f6222590f5f4da5185bb6393a55b23c8247d20
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-21 16:54:57 +0900

    빠진부분 커밋

--- models/userdb.js
+++ models/userdb.js
@@ -1,12 +1,14 @@
-userdb = [{
-    userId: 'admin',
-    userPw: 'admin',
-    userName: '관리자',
-    nickname: '관리관리',
-    gender: '여자',
-    phoneNumber: '01011112222',
-    level: 1,
-    active: 1
-}]
+let userdb = [
+    {
+        userId: 'admin',
+        userPw: 'admin',
+        userName: '관리자',
+        nickname: '관리관리',
+        gender: '여자',
+        phoneNumber: '01011112222',
+        level: 1,
+        active: 1
+    }
+]
 
 module.exports = userdb
