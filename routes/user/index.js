5561c523259465bcd58a76eca0d6a229f9588469
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-21 15:11:38 +0900

    routes>user>index : user 새 index 파일 생성

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const boarddb = require('../../models/userdb')
+const userController = require('./user.controller')
+
+
+router.get('/login',userController.login)
+router.post('/login',userController.login)
+
+router.get('/join',userController.join)
+router.post('/join',userController.join)
+
+router.get('/welcome',userController.welcome)
+
+router.get('/profile',userController.profile)
+
+router.get('/logout',userController.logout)
+
 
 module.exports = router
