f96366bd2986bfbbc3de9b3a1398f083589ae0ef
LeeHwaseob <philar_@naver.com>
2022-02-21 17:06:48 +0900

    admin 라우터 추가

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const user = require('../models/userdb')
 const boardRouter = require('./board/index')
+const adminRouter = require('./admin/index')
 
 
 // const Access = (req,res,next)=>{
@@ -14,13 +15,16 @@ const boardRouter = require('./board/index')
 // }
 
 
-router.get('/',(req,res)=>{
+router.get('/', (req, res) => {
     res.render('index')
 })
 
 
 
 //board 라우터 모음
-router.use('/board',boardRouter)
+router.use('/board', boardRouter)
+
+//admin 라우터 모음
+router.use('/admin', adminRouter)
 
 module.exports = router
