48c62ccfc253bad5e0fe206e8dfcb3409d9451ce
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-21 15:47:25 +0900

    get,post 변수 확정

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -1,29 +1,55 @@
 const express = require('express')
 const router = express.Router()
+const userlist
 
 const login = (req, res) => {
-    res.render('user/login')
+    //로그인 된 사용자는 접근 못함
+    res.render('user/login') //화면 렌더링
+}
+
+const loginCheck = (req, res) => {
+    // 세션 생성
+    res.redirect('/') //홈으로 보내기
 }
 
 const join = (req, res) => {
-    res.render('user/join')
+    //로그인된 사용자는 접근 못함
+    res.render('user/join') //화면 렌더링
+}
+
+const joinCheck = (req, res) => {
+    //로그인된 사용자는 접근 못함
+    res.render('user/join') //화면 렌더링
 }
 
 const welcome = (req, res) => {
-    res.render('user/welcome')
+    //1. 가입만하고 로그인은 되지 않은 상태 or 가입 즉시 로그인이 된 상태
+    // 가입한 회원의 정보를 가져와서 화면에 보이게
+    res.render('user/welcome') //화면 렌더링
 }
 
 const profile = (req, res) => {
-    res.render('user/profile')
+    //로그인한 회원의 정보를 가져와서 화면에 보이도록
+    //로그인한 사용자본인+관리자계정에게만 페이지 보이게
+    res.render('user/profile') //화면 렌더링
 }
 
 const logout = (req, res) => {
-    res.render('user/logout')
+    //로그인했던 사용자 세션 삭제
+    res.render('user/logout')//메인화면으로 보내기
 }
+
+
+// DB 스키마 짜기
+// 필요한 필드
+// --> userId, userPw, userName, nickname, gender, phoneNumber, level, active
+// birth, number항목은 models에 있는 임시 데이터파일에 있는데 그냥 빼주세요.
 
 module.exports = {
     login,
+    loginCheck,
     join,
+    joinCheck,
     welcome,
     profile,
     logout
