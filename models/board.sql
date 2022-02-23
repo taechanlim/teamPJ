9cdb2cb0b66f13b2647005e5fb7f39fc47b527a3
limtaechan <tccap123@naver.com>
2022-02-23 11:19:08 +0900

    게시판 테이블 스키마

---
+++ models/board.sql
@@ -0,0 +1,11 @@
+use team;
+
+CREATE TABLE board(
+    idx int auto_increment,
+    subject varchar(50) not null,
+    nickname varchar(40) not null,
+    content text not null,
+    date timestamp DEFAULT CURRENT_TIMESTAMP not null,
+    hit int not null);
+
+-- INSERT INTO board(subject,nickname,content,hit) values('제목1','별명1','내용1',0)
