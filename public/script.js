a855f48cb558e5db795bf857a93fcaeb4e1598ab
hanbin <hnbn707@gmail.com>
2022-02-23 09:26:26 +0900

    html레이아웃 수정, script 생성

---
+++ public/script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("#header");
+const headerHeight = header.getBoundingClientRect().height;
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > headerHeight / 2) {
+        header.setAttribute("class", "background");
+    } else {
+
+        header.setAttribute("class", "");
+    }
+});
