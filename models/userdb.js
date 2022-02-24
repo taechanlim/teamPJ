7c1614670e262f4d3eeadb03cc5d306e5aa85661
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-24 11:13:38 +0900

    id,pw를 찾는 함수 추가

--- models/userdb.js
+++ models/userdb.js
@@ -14,6 +14,28 @@ let userdb = [
 // 일반 가입자 레벨 1 && 관리자 레벨 2
 // 허용 상태 : 1 && 차단 상태 0
 
+
+//같은 id, pw를 찾음
+function idpwCheck(result,userId,userPw){
+    for(let i=0; i<result.length; i++){
+        if((result[i].userid===userId && result[i].userpw===userPw)==true){
+            return true
+        }
+    }
+}
+
+//같은 id를 찾음
+function idCheck(result,userId){
+    for(let i=0; i<result.length; i++){
+        if(result[i].userid!==undefined){
+            if(result[i].userid===userId)
+            return true
+        }
+    }
+}
+
 module.exports = {
     userdb,
+    idpwCheck,
+    idCheck,
 }
