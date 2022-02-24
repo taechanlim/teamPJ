207512452f0d2b305ae6c536e33f8ab571b7476e
이현진 <hancoco@amm2.cnm7103.co.kr>
2022-02-24 11:14:46 +0900

    엉켜있던 joinCheck, loginCheck, profile 로직 정리, logout 기능 구현

--- routes/user/user.controller.js
+++ routes/user/user.controller.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {userdb} = require('../../models/userdb')
+const {userdb,idpwCheck,idCheck} = require('../../models/userdb')
 const {alertmove} = require('../../util/alert')
 require('dotenv').config()
 const mysql = require('mysql')
@@ -11,29 +11,23 @@ const login = (req, res) => {
     res.render('user/login') //화면 렌더링
 }
 
-const loginCheck = (req, res) => {
+const loginCheck = (req, res, next) => {
     // 세션 생성
     const {userId,userPw} = req.body
     pool.getConnection((err,conn) => {
         conn.query("select userid,userpw from userdb",(error,result)=>{
-            let useridFlag = false
-            for(let i=0; i<result.length; i++){
-                if(result[i].userid===userId && result[i].userpw===userPw){
-                    useridFlag=true;
-                    break;
+            if(!error){
+                //동일한 id,pw를 데이터베이스에서 찾는 함수
+                userFlag = idpwCheck(result,userId,userPw)
+                //동일할 경우, 로그인 가능
+                if (userFlag==true){
+                    req.session.user=req.body
+                    res.redirect('/')
+                } else{
+                    //동일하지 않을 경우, 로그인 불가
+                    res.send(alertmove('/user/login','아이디와 패스워드를 확인해주세요.'))
                 }
-            }
-            if(useridFlag){
-                //로그인 상태 = 1을 저장할 세션 생성
-                req.session.logOn='1'
-                req.session.userInfo=req.body
-                
-                //홈으로 보내기
-                res.redirect('/')
-            } else {
-                //아이디와 패스워드가 틀렸다고 알림뜨기.
-                res.send(alertmove('/user/login','아이디와 패스워드를 확인해주세요.'))
-            }
+            } 
         })
     })
 }
@@ -45,47 +39,42 @@ const join = (req, res) => {
 
 const joinCheck = (req, res) => {
     let {userId,userPw,checkPw,userName,nickname,gender,phoneNumber,level,active} = req.body
-    
     //아무것도 입력하지 않았을때
     let blankFlag = (userId==''||userPw==''||checkPw==''||userName==''||nickname==''||gender==''||phoneNumber=='')
+
     if(blankFlag==true){
         //전부 입력하지 않았을때 출력결과
         res.send(alertmove('/user/join','입력창에 입력을 모두 해주세요'))
     } else {
-    //모두 입력을 했을때 이후 로직
-    let [checkId] = userdb.filter(v=>(v.userId===userId))
-    const regex = /[^0-9]/g; // 숫자가 아닌 문자열을 선택하는 정규식
-    let phoneNumberFix = phoneNumber.replace(regex,"") //숫자외 값은 ''로 처리
+         //모두 입력을 했을때 이후 로직
+        let [checkId] = userdb.filter(v=>(v.userId===userId))
+        const regex = /[^0-9]/g; // 숫자가 아닌 문자열을 선택하는 정규식
+        let phoneNumberFix = phoneNumber.replace(regex,"") //숫자외 값은 ''로 처리
 
-    let userSqlInsert = `INSERT INTO userdb(userId,userPw,userName,nickname,gender,phoneNumber,level,active) VALUES('${userId}','${userPw}','${userName}','${nickname}','${gender}','${phoneNumber}','${level}','${active}')`
+        let userSqlInsert = `INSERT INTO userdb(userId,userPw,userName,nickname,gender,phoneNumber,level,active) VALUES('${userId}','${userPw}','${userName}','${nickname}','${gender}','${phoneNumber}','${level}','${active}')`
 
-    //db에 저장하기 전에 비교하는 로직
-    pool.getConnection((err,conn) => {
-        let idcheckFlag=false
-        conn.query("select userid from userdb",(error,result)=>{
-            for(let i=0; i<result.length; i++){
-                if(result[i].userid!==undefined){
-                    if(result[i].userid===userId)
+        //db에 저장하기 전에 비교하는 로직
+        pool.getConnection((err,conn) => {
+            conn.query("select userid from userdb",(error,result)=>{
+                let userFlag = idCheck(result,userId)
+                if(userFlag==true){
                     // 같은 아이디가 있으면 join으로 돌려보냄
                     res.send(alertmove('/user/join','동일한 아이디가 있습니다. 다른 아이디를 입력해주세요'))
-                }
-            }
-
-            if(userPw!=checkPw){
-                //비밀번호가 서로 같지 않을때 출력결과
-                res.send(alertmove('/user/join','비밀번호가 서로 같지 않습니다. 다시 입력해주세요'))
-            } else if((phoneNumberFix<5)){
-                //전화번호가 5자리 이하를 입력했을때 출력결과
-                res.send(alertmove('/user/join','전화번호를 다시 입력해주세요'))
-            } else{
-                //같은 아이디가 없으면 db에 저장하는 로직
-                conn.query(userSqlInsert,(error,result)=>{
-                    if (!error) {
-                        res.redirect('/user/welcome')
-                        conn.release() //connectionLimit 풀어주는 함수
-                    }
-                    else throw error
-                })
+                } else if(userPw!=checkPw){
+                    //비밀번호가 서로 같지 않을때 출력결과
+                    res.send(alertmove('/user/join','비밀번호가 서로 같지 않습니다. 다시 입력해주세요'))
+                } else if(phoneNumberFix<5){
+                    //전화번호가 5자리 이하를 입력했을때 출력결과
+                    res.send(alertmove('/user/join','전화번호를 다시 입력해주세요'))
+                } else {
+                    //같은 아이디가 없으면 db에 저장하는 로직
+                    conn.query(userSqlInsert,(error,result)=>{
+                        if (!error) {
+                            req.session.user=req.body
+                            res.redirect('/user/welcome')
+                            conn.release() //connectionLimit 풀어주는 함수
+                        }else throw error
+                    })
                 }
             })
         })
@@ -94,8 +83,7 @@ const joinCheck = (req, res) => {
 
 
 const welcome = (req, res) => {
-    //1. 가입만하고 로그인은 되지 않은 상태
-    
+    // 가입만 되고 로그인은 안 된 상태
     // 가입한 회원의 정보를 가져와서 화면에 보이게
     const {user} = req.session
 
@@ -120,11 +108,11 @@ const profile = (req, res) => {
             }
             if(useridFlag===true){
                 //프로필 접근 가능
-                console.log('성공')
+                // console.log('성공')
             }else{
                 //프로필 접근 불가
                 //로그인이 필요한 메뉴입니다라고 알람?
-                console.log('실패')
+                // console.log('실패')
             }
         })
     })
@@ -134,9 +122,9 @@ const profile = (req, res) => {
 
 const logout = (req, res) => {
     //로그인했던 사용자 세션 삭제
-    // req.session.destroy(() => {
-    //     req.session;
-    //   });
+    req.session.destroy(() => {
+        req.session;
+      });
     res.redirect('/')//메인화면으로 보내기
 }
 
