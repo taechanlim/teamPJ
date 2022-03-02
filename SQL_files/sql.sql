0c6b05056dbfeaeae42cf66978664725a2c7e4e4
limtaechan <tccap123@naver.com>
2022-03-02 12:39:37 +0900

    팀플

--- SQL_files/sql.sql
+++ SQL_files/sql.sql
@@ -17,3 +17,17 @@ CREATE TABLE board(
 -- UPDATE board SET idx = @COUNT:=@COUNT+1;
 -- select subject,nickname,content,DATE_FORMAT(date,'%Y-%m-%d %h:%m:%s') as date,hit from board; 게시판 리스트 시간표시
 
+-- 경로복사&붙여넣기하면 적용됨
+-- opt+cmd+c
+-- use userSql;
+
+CREATE TABLE userdb(
+    userid varchar(15) not null primary key,
+    userpw varchar(15) not null,
+    username varchar(15) not null,
+    nickname varchar(15) not null,
+    gender varchar(15) not null,
+    phoneNumber int(15) not null,
+    level int(1) not null,
+    active int(1) not null
+);
